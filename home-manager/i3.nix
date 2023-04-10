efb012aa1f1d669ca79b45fac429c94cf07fc0fc
Jan Riedel <git@rjan.dev>
2023-04-10 18:51:34 +0200

    update xrandr settings to work with proprietary nvidia drivers

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -37,7 +37,7 @@ in {
     #exec_always --no-startup-id "xrandr --setprovideroutputsource modesetting NVIDIA-0 && xrandr --auto && xrandr --output DP-1 --refresh 144 && xrandr --output DVI-I-1 --right-of DP-1 && xrandr --output VGA-1-1 --left-of DP-1 --rotation left"
 
     # set screen resolution
-    exec_always --no-startup-id "xrandr --auto && xrandr --output DP-1 --refresh 120 && xrandr --output DP-2 --right-of DP-1 && xrandr --output HDMI-1 --left-of DP-1"
+    exec_always --no-startup-id "xrandr --auto && xrandr --output DP-4 --refresh 120 && xrandr --output DP-3 --right-of DP-4 && xrandr --output HDMI-0 --left-of DP-4"
 
     # background image
     # exec_always --no-startup-id "nitrogen --restore"

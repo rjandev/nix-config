e849199216b32570079e0c49c8d410daa2b8f7d0
Jan Riedel <git@rjan.dev>
2023-05-14 20:02:29 +0200

    Add 1password cli

--- nixos/1password-configuration.nix
+++ nixos/1password-configuration.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  programs._1password.enable = true;
+  programs._1password.package = pkgs.unstable._1password;
   programs._1password-gui.enable = true;
   programs._1password-gui.polkitPolicyOwners = [ "jan" ];
 }

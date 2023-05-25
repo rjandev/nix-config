709b31856709c45740c9843f434c8dfd7c3cc16b
Jan Riedel <git@rjan.dev>
2023-05-25 10:40:20 +0200

    Configure 1Password to use correct SSH key for GitHub

--- home-manager/ssh-configuration.nix
+++ home-manager/ssh-configuration.nix
@@ -2,8 +2,19 @@
 
 let
   sshConfig = ''
+
+    Host github.com
+      HostName github.com
+      User git
+      IdentityFile ~/.ssh/github-ssh.pub
+      IdentitiesOnly yes
+
     Host *
-    IdentityAgent ~/.1password/agent.sock
+      IdentityAgent ~/.1password/agent.sock
   '';
 
-in { home.file.".ssh/config".text = sshConfig; }
+in {
+  home.file.".ssh/config".text = sshConfig;
+  home.file.".ssh/github-ssh.pub" = { source = ./github-ssh.pub; };
+}
+

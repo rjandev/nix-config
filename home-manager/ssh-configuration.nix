e8f5942fff042a82271cc7e2d00bd0c1f14867f6
Jan Riedel <git@rjan.dev>
2023-07-09 20:10:19 +0200

    Configure 1Password to use correct SSH key for GitLab

--- home-manager/ssh-configuration.nix
+++ home-manager/ssh-configuration.nix
@@ -9,6 +9,12 @@ let
       IdentityFile ~/.ssh/github-ssh.pub
       IdentitiesOnly yes
 
+    Host gitlab.com
+          HostName gitlab.com
+          User git
+          IdentityFile ~/.ssh/gitlab-ssh.pub
+          IdentitiesOnly yes
+
     Host *
       IdentityAgent ~/.1password/agent.sock
   '';
@@ -16,5 +22,6 @@ let
 in {
   home.file.".ssh/config".text = sshConfig;
   home.file.".ssh/github-ssh.pub" = { source = ./github-ssh.pub; };
+  home.file.".ssh/gitlab-ssh.pub" = { source = ./gitlab-ssh.pub; };
 }
 

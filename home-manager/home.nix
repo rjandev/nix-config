ad39bb28ce0bf252736d8886ad05476fab1acf52
Jan Riedel <git@rjan.dev>
2023-12-27 14:20:30 +0100

    remove problematic fzf packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -78,23 +78,11 @@
     };
   };
 
-  home.packages = [ # #
-    pkgs.fzf
-    pkgs.fzf-obc
-    pkgs.fzf-git-sh
-  ];
+  programs.fzf = { enable = true; };
 
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    bashrcExtra = ''
-      if command -v fzf-share >/dev/null; then
-        source "$(fzf-share)/key-bindings.bash"
-        source "$(fzf-share)/completion.bash"
-        source "${pkgs.fzf-git-sh}/share/fzf-git-sh/fzf-git.sh"
-        source "${pkgs.fzf-obc}/share/fzf-obc/bin/fzf-obc.bash"
-      fi
-    '';
   };
 
 }

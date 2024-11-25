0de3f603c88c35af4a62e3b5e0cc0c12d5dc6f8a
Jan Riedel <git@rjan.dev>
2024-11-25 21:12:19 +0100

    Update 1password package name

--- nixos/1password-configuration.nix
+++ nixos/1password-configuration.nix
@@ -2,7 +2,7 @@
 
 {
   programs._1password.enable = true;
-  programs._1password.package = pkgs.unstable._1password;
+  programs._1password.package = pkgs.unstable._1password-cli;
   programs._1password-gui.enable = true;
   programs._1password-gui.polkitPolicyOwners = [ "jan" ];
 }
